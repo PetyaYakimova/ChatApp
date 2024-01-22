f862169dd3e7ef69b5cb794364576d92d7e2752b
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-22 08:17:57 +0200

    Add project files.

---
+++ ChatApp/Models/Message/MessageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.Models.Message
+{
+	public class MessageViewModel
+	{
+		public string Sender { get; set; } = string.Empty;
+
+		public string MessageText { get; set; } = string.Empty;
+    }
+}
